8655d02798cd0a9a15e0059e108347b981b102e7
Ao1Pointblank <88149675+Ao1Pointblank@users.noreply.github.com>
2024-04-08 21:03:01 -0400

    Add Normcap OCR Integration

--- zenity-translate.sh
+++ zenity-translate.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-#dependencies: wmctrl xclip translate-shell zenity notify-send
+#DEPENDENCIES: 
+#wmctrl xclip translate-shell zenity notify-send
+#
+#Normcap from Flathub: (for OCR screen selection)
+#https://dynobo.github.io/normcap/
 
 #the language which selected words will be translated into (your native language)
 #use ``trans -R`` to get a full list of language codes
@@ -9,7 +13,14 @@ TARGET_LANGCODE=en
 wmctrl -c "Translation";
 
 TEMPFILE=$(mktemp);
-xclip -o > $TEMPFILE
+
+#prefer current selection as input but fallback to clipboard
+if [[ -n "$(xclip -o -selection primary])" ]]; then
+    xclip -o -selection primary > $TEMPFILE
+else
+    xclip -o -selection clipboard > $TEMPFILE
+fi
+
 
 FILTERED_TEXT=$(cat "$TEMPFILE" | tr '\r\n' ' ' | tr '`' "'" )
 DETECTED_LANG=$(echo "$FILTERED_TEXT" | trans -j -identify -no-ansi | grep --color=never "Name" | awk '{$1=""; sub(/^[ \t]+/, ""); print $0}')
@@ -25,6 +36,7 @@ OUTPUT=$(zenity  --title="Translation - $DETECTED_LANG Detected" \
         --text="$(echo "$FILTERED_TEXT" | trans -brief -j -target $TARGET_LANGCODE )" \
         --ok-label=Close \
         --icon-name=document-export \
+        --extra-button "Screen Select" \
         --extra-button "$DETECTED_LANG Pronunciation" \
         --extra-button "Speak Translation" \
         --extra-button "Interactive Shell" &
@@ -42,14 +54,29 @@ if [[ "$OUTPUT" == "ZENITY_TRANSLATE_TEXT_TOO_LONG" ]] ; then
 fi
 
 #what the extra buttons do
-if [[ "$OUTPUT" == "$DETECTED_LANG Pronunciation" ]] ; then
-	echo "$FILTERED_TEXT" | trans -brief -j -speak
+if [[ "$OUTPUT" == "Screen Select" ]] ; then
+	#run OCR tool, which saves selected text to xclip clipboard, then run translate-zenity again
+	/usr/bin/flatpak run com.github.dynobo.normcap &&
+	$0
 else
-	if [[ "$OUTPUT" == "Speak Translation" ]] ; then
-		echo "$FILTERED_TEXT" | trans -brief -j -target "$TARGET_LANGCODE" -play
-	fi;
-	if [[ "$OUTPUT" == "Interactive Shell" ]] ; then
-		gnome-terminal -- bash -c 'echo "Language Codes:" ; trans -R ; echo "Usage: source-lang:target-lang, Example: en:de" ; trans -shell -brief -join-sentence -play'
+	if [[ "$OUTPUT" == "$DETECTED_LANG Pronunciation" ]] ; then
+		echo "$FILTERED_TEXT" | trans -brief -j -speak
+	else
+		if [[ "$OUTPUT" == "Speak Translation" ]] ; then
+			echo "$FILTERED_TEXT" | trans -brief -j -target "$TARGET_LANGCODE" -play
+		fi;
+		if [[ "$OUTPUT" == "Interactive Shell" ]] ; then
+			cd '/home/pointblank/.translate'
+			gnome-terminal --wait -- bash -c '
+				echo "Language Codes:" ;
+				trans -R ;
+				echo "Usage: source-lang:target-lang, Example: en:de" ;
+				trans -shell -brief -join-sentence -play -download-audio'
+			for ts_file in *.ts; do
+    			mp3_file="${ts_file%.ts}.mp3"
+    			mv "$ts_file" "$mp3_file"
+			done
+		fi;
 	fi;
 fi;
 rm ${TEMPFILE}
